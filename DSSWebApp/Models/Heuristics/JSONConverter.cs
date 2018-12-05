ad16e8d12406737e7afe1fa80446ceb002716732
Tirocigno <fede.naldini1@gmail.com>
2018-12-05 13:39:13 +0100

    Changed deserializeGAP method

--- DSSWebApp/Models/Heuristics/JSONConverter.cs
+++ DSSWebApp/Models/Heuristics/JSONConverter.cs
@@ -9,20 +9,23 @@ namespace DSSWebApp.Models.Heuristics
 {
     public class JSONConverter
     {
-        public void deserializeGAP(string path)
+        public static GAPInstance deserializeGAP(string path)
         {
             StreamReader fin;
             String jstring;
+            GAPInstance ist;
             try
             {
                 fin = new StreamReader(path);
                 jstring = fin.ReadToEnd();
-                GAPInstance ist = JsonConvert.DeserializeObject<GAPInstance>(jstring);
+                ist = JsonConvert.DeserializeObject<GAPInstance>(jstring);
             }
             catch (Exception ex)
             {
-
+                ist = new GAPInstance();
             }
+
+            return ist;
         }
     }
 }
