ae44996e7aca552568fd7582791c7109f7dd83b3
Tirocigno <fede.naldini1@gmail.com>
2018-12-12 15:36:10 +0100

    Will solve stack overflow with multiThreading

--- DSSWebApp/Controllers/ControllerClass.cs
+++ DSSWebApp/Controllers/ControllerClass.cs
@@ -1,9 +1,11 @@
 ﻿using DSSWebApp.Models.Database;
 using DSSWebApp.Models.Heuristics;
+using RDotNet;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http; //Necessaria al funzionamento di tutto.
 
@@ -72,6 +74,33 @@ namespace DSSWebApp.Controllers
                 case "serie": return JSONConverter.serialize(dbConnection.readSerieFromDB());
                 default: return "Error, no db found";
             }
+        }
+
+        [HttpGet]
+        [ActionName("prevision")]
+        public string executePrevision()
+        {
+            //  Thread t = new Thread(rCalculus, 2500000 /*thread stack size of 2.5MB*/);
+            //t.Start();
+            rCalculus();
+            return "Done";
+        }
+
+        private void rCalculus()
+        {
+            StartupParameter rinit = new StartupParameter();
+            rinit.Quiet = true;
+            rinit.RHome = "C:\\Program Files\\R\\R-3.4.4";
+            rinit.Interactive = true;
+            REngine.SetEnvironmentVariables();
+            REngine engine = REngine.GetInstance(null, true, rinit);
+            var x = engine.Evaluate("x <- 1 + 2");
+            Console.WriteLine(x);
+            engine.Evaluate("library(tseries)");
+            engine.Evaluate("library(forecast)");
+            Console.WriteLine("Arrivato in fondo");
+            var y = engine.Evaluate("y <- 4 + 7");
+            Console.WriteLine(y);
         }
 
 
